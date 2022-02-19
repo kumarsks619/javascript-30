dd7d8620899951a59bddb57c41b8be2388b7af24
VeNoM <kumarsks619@gmail.com>
2022-02-19 21:11:35 +0530

    11 - Mouse Move Shadow

---
+++ 11 - Mouse Move Shadow/script.js
@@ -0,0 +1,17 @@
+const hero = document.querySelector('.hero')
+const text = hero.querySelector('h1')
+const walk = 30 // 30px (shadow range ---> (-15px, +15px))
+
+hero.addEventListener('mousemove', function (e) {
+    const { offsetWidth: width, offsetHeight: height } = hero
+    let { offsetX: x, offsetY: y } = e
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft
+        y = y + e.target.offsetTop
+    }
+
+    const xWalk = Math.round((x / width) * walk - walk / 2)
+    const yWalk = Math.round((y / height) * walk - walk / 2)
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,0,0,0.3)`
+})
