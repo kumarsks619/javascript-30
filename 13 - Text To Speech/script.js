ead538d7cf7ab7bea5f5e12c47676c1f30b1f4ed
VeNoM <kumarsks619@gmail.com>
2022-03-13 20:48:38 +0530

    13 - Text To Speech

---
+++ 13 - Text To Speech/script.js
@@ -0,0 +1,33 @@
+const inView = document.querySelector('.in-view')
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+recognition.interimResults = true
+
+let p = document.createElement('p')
+const words = document.querySelector('.words')
+words.appendChild(p)
+
+recognition.addEventListener('result', (e) => {
+    const transcript = Array.from(e.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('')
+
+    inView.scrollIntoView()
+    p.textContent = transcript
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p')
+        words.appendChild(p)
+    }
+
+    if (transcript.includes('open my portfolio')) {
+        window.open('https://www.shubhamm.in/', '_blank')
+    }
+})
+
+recognition.addEventListener('end', recognition.start)
+
+recognition.start()
